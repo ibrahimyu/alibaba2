65b4e574a35f4d09d89f0aa908db179e55e9627f
Mamen014 <ryaniaska14@gmail.com>
2025-05-24 21:36:04 +0700

    move input preview

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,10 +8,12 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
       },
       '/output': {
         target: 'http://localhost:3000',
         changeOrigin: true,
+        rewrite: path => path.replace(/^\/output/, '')
       }
     }
   }
